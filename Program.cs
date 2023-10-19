c2da47b13e586aa9c539c4a26afcc65eeb102402
douglas <thedouglas329@gmail.com>
2023-10-19 14:33:24 -0300

    Implementando reserva de hospedagem

--- Program.cs
+++ Program.cs
@@ -21,5 +21,27 @@ reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Suite: {suite.TipoSuite}");
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
 Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+
+//Aumenta a quantidade de dias para obter desconto no valor total
+reserva.DiasReservados = 10;
+
+Console.WriteLine($"Suite com desconto: {suite.TipoSuite}");
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+
+// Cria a suíte simples
+Suite suiteSimples = new Suite(tipoSuite: "Simples", capacidade: 1, valorDiaria: 30);
+
+// Cria uma nova reserva com hospedes acima da capacidade, passando a suíte e os hóspedes
+Reserva reservaSimples = new Reserva(diasReservados: 5);
+reservaSimples.CadastrarSuite(suiteSimples);
+
+// Vai lançar a exception pois não pode ter mais hospedes que a suite comporta
+reservaSimples.CadastrarHospedes(hospedes);
+
+Console.WriteLine($"Suite com desconto: {suiteSimples.TipoSuite}");
+Console.WriteLine($"Hóspedes: {reservaSimples.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reservaSimples.CalcularValorDiaria()}");
